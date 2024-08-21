f09c908a92581f261db42821d8609cea7108cd6e
Sidhant <sidhant1593@gmail.com>
2024-08-22 00:58:22 +0530

    source code link added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
-import { Navigate, Routes, Route } from 'react-router-dom';
+import { Navigate, Routes, Route, Link } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
+import { Github } from 'lucide-react';
 
 import LoadingSpinner from './components/LoadingSpinner';
 import FloatingShape from './components/FloatingShape';
@@ -52,6 +53,13 @@ function App() {
             <FloatingShape color="bg-green-500" size="w-64 h-64" top="-5%" left="10%" delay={0} />
             <FloatingShape color="bg-emerald-500" size="w-48 h-48" top="70%" left="80%" delay={5} />
             <FloatingShape color="bg-lime-500" size="w-32 h-32" top="40%" left="-10%" delay={2} />
+            <Link
+                to="https://github.com/sidhant2709/mern-auth"
+                className="absolute bottom-0 right-0 p-4 bg-gray-800 bg-opacity-50 rounded-full border border-gray-700 text-green-500"
+                title="Source Code on GitHub"
+            >
+                <Github className="size-6 inline-block" />
+            </Link>
 
             <Routes>
                 <Route
